2952b0c6bafb2f6453a63cc50c916bf07a035c3d
Leticia Téllez <letytellez06@hotmail.com>
2022-10-25 21:23:45 -0600

    table configurations

---
+++ Hiring/Hiring.Data/Configurations/ApplicationConfig.cs
@@ -0,0 +1,26 @@
+﻿using Hiring.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hiring.Data.Configurations
+{
+    public class ApplicationConfig
+    {
+        public void Configure(EntityTypeBuilder<Application> entity)
+        {
+            entity.ToTable("Application");
+
+            entity.HasOne(d => d.Applicant)
+                .WithMany(p => p.Applications)
+                .HasForeignKey(d => d.ApplicantId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__application_applicant_id");
+
+            entity.HasOne(d => d.Vacant)
+                .WithMany(p => p.Applications)
+                .HasForeignKey(d => d.VacantId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__application_vacant_id");
+        }
+    }
+}
---
+++ Hiring/Hiring.Data/Configurations/VacantConfig.cs
@@ -0,0 +1,26 @@
+﻿using Hiring.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hiring.Data.Configurations
+{
+    public class VacantConfig
+    {
+        public void Configure(EntityTypeBuilder<Vacant> entity)
+        {
+            entity.ToTable("Vacant");
+
+            entity.HasOne(d => d.Company)
+                .WithMany(p => p.Vacants)
+                .HasForeignKey(d => d.CompanyId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__vacant_company_id");
+
+            entity.HasOne(d => d.Position)
+                .WithMany(p => p.Vacants)
+                .HasForeignKey(d => d.PositionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__vacant_position_id");
+        }
+    }
+}
